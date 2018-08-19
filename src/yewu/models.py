33fa2e9449274da2184f7996f92273c5c785305b
coblan <coblan163.com>
2018-08-20 00:49:05 +0800

    开发

--- src/yewu/models.py
+++ src/yewu/models.py
@@ -5,8 +5,13 @@ from django.db import models
 class Yewu(models.Model):
     name = models.CharField('标识名称',max_length=30)
     title = models.CharField('主标题',max_length=200)
+    sub_title=models.CharField('副标题',max_length=600)
+    
     
 
 
 class Yewuer(models.Model):
-    pass
+    name = models.CharField('人名',max_length=30)
+    head=models.CharField('头像',max_length=300)
+    phone = models.CharField('手机号码',max_length=50)
+    
---
+++ src/yewu/structure.py
@@ -0,0 +1,13 @@
+structure = [
+    [{'name':'gongshizhuche','label':'公司注册','yewu':[
+            {'name':'yewu_gongshizhuche','label':'公司注册'},
+            {'name':'yewu_zhuchedizhi','label':'注册地址'},
+            {'name':'yewu_dizhixufei','label':'地址续费'},
+            {'name':'yewu_yinghangkaihu','label':'银行开户'},
+        ]},
+     {"name":'company_change','label':'公司变更','yewu':[
+            {'name':'yewu_company_change_name',"label":'公司名称变更'},
+            {'name':'faren_zhiben_change','label':'法人,高管或注册资本变更'}
+        ]}
+     ]
+]
