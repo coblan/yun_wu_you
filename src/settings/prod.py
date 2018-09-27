b9797b53189026124b8a9d8ed510e8a4bcf4dcee
he_yulin <he_yulin@163.com>
2018-09-27 13:23:42 +0800

    配置

---
+++ src/settings/prod.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'NAME': 'yun_wu_you',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'root123456789',
+        'HOST': '127.0.0.1', 
+        'PORT': '3306',        
+      },
+    }
+
+ALLOWED_HOSTS = ['yzdd360.com']
