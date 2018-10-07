df50aa2f7023241dfdad184692dfea0e96a937a1
he_yulin <he_yulin@163.com>
2018-10-07 16:50:37 +0800

    debug

--- src/webpage/js/com/rich_saler.js
+++ src/webpage/js/com/rich_saler.js
@@ -7,11 +7,21 @@ Vue.component('com-rich-saler',{
         <div>
             <label v-text="saler.name"></label>
 
-            <button class="btn btn-default btn-sm">免费电话咨询</button>
+            <button class="btn btn-default btn-sm" @click="free_phone()">免费电话咨询</button>
         </div>
         <hr>
         <div v-text="saler.slogan" style="text-align: left">
         </div>
 
-    </div>`
+    </div>`,
+    methods:{
+        free_phone:function(){
+            var win=pop_layer({},'com-free-phone',function(){
+                layer.close(win)
+            },{
+                title:'免费电话咨询',
+                area:['540px','360px']
+            })
+        }
+    }
 })
